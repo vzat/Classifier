5f00efe3fbb241301ae13790d788bf046ef3474e
Vlad Zat <vlad.zat@mydit.ie>
2018-04-17 23:08:01 +0100

    Train and Test dataset

--- classifier.py
+++ classifier.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 # Read training set
 columnHeadings=['id', 'age', 'job', 'marital', 'education', 'default', 'balance', 'housing', 'loan',
@@ -11,27 +13,53 @@ columnHeadings=['id', 'age', 'job', 'marital', 'education', 'default', 'balance'
 
 trainingset = pd.read_csv('trainingset.txt', header=None, names=columnHeadings, index_col=False, na_values=['unknown'])
 
+# Extract target feature
+targetLabels = trainingset['y']
+
 # Extract numeric features
 numeric_features = ['age', 'balance', 'day', 'duration', 'campaign', 'pdays', 'previous']
 numeric_dfs = trainingset[numeric_features]
 
 # Extract categorical features
-cat_dfs = trainingset.drop(numeric_features, axis=1)
+# ID is removed as it is a unique value which will not help the classification
+# The target feature is removed as well
+cat_dfs = trainingset.drop(numeric_features + ['id'] + ['y'], axis=1)
 
-c_cat_dfs = pd.get_dummies(cat_dfs)
+# Fill missing values with unknown
+cat_dfs.fillna('unknown', inplace=True)
 
-# Create manual python dictonary
+# Replace missing values if they occur less than 10% of the time
+for column in cat_dfs:
+    job_freq = cat_dfs[column].value_counts(ascending=False)
+    job_freq_dict = job_freq.to_dict()
+    if 'unknown' in job_freq_dict:
+        no_unknown_values = job_freq_dict['unknown']/len(cat_dfs[column]) * 100.0
 
-# # Convert categorical data to dictionaries
-# cat_dfs = cat_dfs.T.to_dict().values()
-#
-# # Convert categorical values to numeric which are needed for sklearn
-# # vecObj = DictVectorizer(sparse=False)
-# le = LabelEncoder()
-# vec_cat_dfs = le.fit_transform(cat_dfs)
-#
-# print(vec_cat_dfs)
+        # Impute values that are missing less than 10 percent of values
+        most_freq_key = job_freq.keys()[0]
+        if most_freq_key != 'unknown' and no_unknown_values < 10:
+            cat_dfs[column].replace(to_replace='unknown', value=most_freq_key, inplace=True)
+
+# Convert categorical data to dictionaries
+cat_dfs = cat_dfs.T.to_dict().values()
+
+# Convert categorical values to numeric which are needed for sklearn
+vecObj = DictVectorizer(sparse=False)
+vec_cat_dfs = vecObj.fit_transform(cat_dfs)
 
 # Merge numeric and categorical features into one set
-# train_dfs = np.hstack((numeric_dfs.as_matrix(), vec_cat_dfs))
+train_dfs = np.hstack((numeric_dfs.as_matrix(), vec_cat_dfs))
+
+# Create Decision Tree
+# treeModel = DecisionTreeClassifier(criterion='entropy')
+# treeModel.fit(train_dfs, targetLabels)
+
+# Test decision tree
+train_values, test_values, train_target, test_target = train_test_split(train_dfs, targetLabels, test_size=0.4)
+treeModel = DecisionTreeClassifier(criterion='entropy')
+treeModel.fit(train_values, train_target)
+predictions = treeModel.predict(test_values)
+print("Accuracy= " + str(accuracy_score(test_target, predictions, normalize=True)))
+
+
 
