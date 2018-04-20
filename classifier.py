a1927148cb379b4864c57cacdb4cfdf3d74e0049
Vlad Zat <vlad.zat@mydit.ie>
2018-04-21 00:38:21 +0100

    Add documentation

--- classifier.py
+++ classifier.py
@@ -36,6 +36,7 @@ numeric_queries_dfs = queries[numeric_features]
 # Clamp the outlier on the balance
 maxBalance = int(np.mean(numeric_dfs['balance']) + 2 * np.std(numeric_dfs['balance']))
 numeric_dfs.loc[numeric_dfs['balance'] > maxBalance, 'balance'] = maxBalance
+numeric_queries_dfs.loc[numeric_queries_dfs['balance'] > maxBalance, 'balance'] = maxBalance
 
 
 # Extract categorical features
@@ -78,26 +79,26 @@ train_dfs = np.hstack((numeric_dfs.as_matrix(), vec_cat_dfs))
 queries_dfs = np.hstack((numeric_queries_dfs.as_matrix(), vec_cat_queries_dfs))
 
 
-# Test Decision Tree
-train_values, test_values, train_target, test_target = train_test_split(train_dfs, targetLabels, test_size=0.4)
-testDecisionTreeClassifier = DecisionTreeClassifier(criterion='entropy')
-testDecisionTreeClassifier.fit(train_values, train_target)
-predictions = testDecisionTreeClassifier.predict(test_values)
-print("Decision Tree Accuracy = " + str(accuracy_score(test_target, predictions, normalize=True)))
-
-# Test KNN
-train_values, test_values, train_target, test_target = train_test_split(train_dfs, targetLabels, test_size=0.4)
-knn = KNeighborsClassifier(n_neighbors=11)
-knn.fit(train_values, train_target)
-predictions = knn.predict(test_values)
-print("KNN Accuracy = " + str(accuracy_score(test_target, predictions, normalize=True)))
-
-# Test Gaussian Naive Bayes
-train_values, test_values, train_target, test_target = train_test_split(train_dfs, targetLabels, test_size=0.4)
-gnb = GaussianNB()
-gnb.fit(train_values, train_target)
-predictions = gnb.predict(test_values)
-print("Naive Bayes Accuracy = " + str(accuracy_score(test_target, predictions, normalize=True)))
+# # Test Decision Tree
+# train_values, test_values, train_target, test_target = train_test_split(train_dfs, targetLabels, test_size=0.4)
+# testDecisionTreeClassifier = DecisionTreeClassifier(criterion='entropy')
+# testDecisionTreeClassifier.fit(train_values, train_target)
+# predictions = testDecisionTreeClassifier.predict(test_values)
+# print("Decision Tree Accuracy = " + str(accuracy_score(test_target, predictions, normalize=True)))
+#
+# # Test KNN
+# train_values, test_values, train_target, test_target = train_test_split(train_dfs, targetLabels, test_size=0.4)
+# knn = KNeighborsClassifier(n_neighbors=11)
+# knn.fit(train_values, train_target)
+# predictions = knn.predict(test_values)
+# print("KNN Accuracy = " + str(accuracy_score(test_target, predictions, normalize=True)))
+#
+# # Test Gaussian Naive Bayes
+# train_values, test_values, train_target, test_target = train_test_split(train_dfs, targetLabels, test_size=0.4)
+# gnb = GaussianNB()
+# gnb.fit(train_values, train_target)
+# predictions = gnb.predict(test_values)
+# print("Naive Bayes Accuracy = " + str(accuracy_score(test_target, predictions, normalize=True)))
 
 
 # Classify queries using decision trees
